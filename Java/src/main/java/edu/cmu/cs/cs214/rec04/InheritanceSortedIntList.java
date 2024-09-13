9995a129af034458f40c9473f7561ec195960ab3
Zhixin Orange <120090222@link.cuhk.edu.cn>
2024-09-13 00:42:23 -0400

    Implement InheritanceSortedIntlist by extending SortedIntList and overriding add and addall

--- Java/src/main/java/edu/cmu/cs/cs214/rec04/InheritanceSortedIntList.java
+++ Java/src/main/java/edu/cmu/cs/cs214/rec04/InheritanceSortedIntList.java
@@ -6,11 +6,53 @@ package edu.cmu.cs.cs214.rec04;
  * with the current size, which goes down when an element is removed)
  * and exports an accessor (totalAdded) for this count.
  *
- * @author Nora Shoemaker
+ * @author Zhixin Chen
  *
  */
 
-public class InheritanceSortedIntList {
-    // Write your implementation below with API documentation
+public class InheritanceSortedIntList extends SortedIntList{
+    private int totalAdded;
+
+    public InheritanceSortedIntList() {
+        totalAdded = 0;
+    }
+
+    /**
+     * Adds the specified int to the list, meanwhile record the number of the adds.
+     *
+     * @param num an integer to be added to the list
+     * @return true if the list is changed as a result of the call
+     */
+    @Override
+    public boolean add(int num) {
+      this.totalAdded++;
+      return super.add(num);
+    }
+
+    /**
+     * Adds all of the elements of the IntegerList to the list.
+     *
+     * @param list IntegerList containing elements to be added to the list
+     * @return true if the list changed as a result of the call
+     */
+    @Override
+    public boolean addAll(IntegerList list) {
+        boolean status = true;
+        for (int i = 0; i < list.size(); i++) {
+            status &= this.add(list.get(i));
+            if(!status) return false;  
+        }
+        return true;    
+    }
+
+    /**
+     * Fetch the number of the total adds.
+     *
+     * @return the number of total adds.
+     */
+   public int getTotalAdded(){
+    System.out.printf("%d", this.totalAdded);
+    return this.totalAdded;
+}
 
 }
